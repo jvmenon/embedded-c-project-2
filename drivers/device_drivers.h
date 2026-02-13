1182eccf3f15144b4c2223deff866bff808de9be
Jyothish Vijay <37020333+jvmenon@users.noreply.github.com>
2026-02-13 18:19:53 +0530

    Import from Refringence: drivers/device_drivers.h

---
+++ drivers/device_drivers.h
@@ -0,0 +1,10 @@
+// Copyright Refringence
+// Built with Refringence IDE — https://refringence.com
+#ifndef DEVICE_DRIVERS_H
+#define DEVICE_DRIVERS_H
+
+#include <stdbool.h>
+
+bool drivers_init(void);
+
+#endif // DEVICE_DRIVERS_H
