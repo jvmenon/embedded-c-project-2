fbdffb4e7f2a2a7802aa9465cf7d155a29454418
Jyothish Vijay <37020333+jvmenon@users.noreply.github.com>
2026-02-13 18:19:52 +0530

    Import from Refringence: drivers/application_processor.h

---
+++ drivers/application_processor.h
@@ -0,0 +1,10 @@
+// Copyright Refringence
+// Built with Refringence IDE — https://refringence.com
+#ifndef APPLICATION_PROCESSOR_H
+#define APPLICATION_PROCESSOR_H
+
+#include <stdbool.h>
+
+bool app_processor_init(void);
+
+#endif // APPLICATION_PROCESSOR_H
